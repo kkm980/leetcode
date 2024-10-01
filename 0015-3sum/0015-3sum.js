3a73761e7594c74425f3cedcc75b0dfb7328c86d
KRISHNA KANT <77038999+kkm980@users.noreply.github.com>
2024-10-01 23:59:44 +0530

    Time: 170 ms (35.44%), Space: 66.4 MB (19.74%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -4,29 +4,37 @@
  */
 var threeSum = function(nums) {
 nums.sort((a, b) => a - b);
-    let result = [];
+   let result = [];
 
-    for (let i = 0; i < nums.length - 2; i++) {
-        if (i > 0 && nums[i] === nums[i - 1]) continue;
+for (let i = 0; i < nums.length - 2; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) {
+        continue;
+    }
 
-        let left = i + 1, right = nums.length - 1;
+    let left = i + 1;
+    let right = nums.length - 1;
 
-        while (left < right) {
-            let sum = nums[i] + nums[left] + nums[right];
+    while (left < right) {
+        const sum = nums[i] + nums[left] + nums[right];
 
-            if (sum === 0) {
-                result.push([nums[i], nums[left], nums[right]]);
-                while (nums[left] === nums[left + 1]) left++;
-                while (nums[right] === nums[right - 1]) right--;
+        if (sum === 0) {
+            result.push([nums[i], nums[left], nums[right]]);
+            while (left < right && nums[left] === nums[left + 1]) {
                 left++;
-                right--;
-            } else if (sum < 0) {
-                left++;
-            } else {
+            }
+            while (left < right && nums[right] === nums[right - 1]) {
                 right--;
             }
+            left++;
+            right--;
+        } else if (sum < 0) {
+            left++;
+        } else {
+            right--;
         }
     }
+}
+
 
     return result;
 };
