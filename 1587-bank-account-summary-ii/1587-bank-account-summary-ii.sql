1a2d0c7dd70790f780292ff12b13b0e0fc46ba6c
KRISHNA KANT <77038999+kkm980@users.noreply.github.com>
2024-10-27 19:26:02 +0530

    Time: 1013 ms (32.53%), Space: 0B (100.00%) - LeetHub

--- 1587-bank-account-summary-ii/1587-bank-account-summary-ii.sql
+++ 1587-bank-account-summary-ii/1587-bank-account-summary-ii.sql
@@ -2,5 +2,5 @@
 SELECT u.name, SUM(t.amount) AS balance
 FROM Users AS u
 LEFT JOIN Transactions AS t ON u.account = t.account
-GROUP BY u.account
+GROUP BY u.account, u.name
 HAVING SUM(t.amount) > 10000;
