2515241902aea0c9642b00f0ab70cf3a72966f55
KRISHNA KANT <77038999+kkm980@users.noreply.github.com>
2024-10-27 21:30:30 +0530

    Time: 414 ms (54.14%), Space: 0B (100.00%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
-SELECT p2.email as Email
+SELECT p1.email as Email
 FROM Person as p1
 JOIN Person as p2
 ON p1.email = p2.email
 WHERE p1.id != p2.id
-GROUP BY p2.email;
+GROUP BY p1.email;
