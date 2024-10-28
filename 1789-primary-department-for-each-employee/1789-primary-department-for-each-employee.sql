8a586827c5442c6075817ccc12e0f810ea0913d9
KRISHNA KANT <77038999+kkm980@users.noreply.github.com>
2024-10-29 00:45:23 +0530

    Time: 1321 ms (5.04%), Space: 0B (100.00%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -9,3 +9,20 @@ FROM Employee
 GROUP BY employee_id;
 
 
+# NOTE->
+# SELECT 
+#     employee_id,
+#     COALESCE(
+#         CASE WHEN primary_flag = 'Y' THEN department_id END,
+#         department_id
+#     ) AS department_id
+# FROM Employee
+# GROUP BY employee_id;
+# # IS ALSO CORRECT BUT IT I SHOWING ERROR FOR --->
+# INPUT
+# {"headers":{"Employee":["employee_id","department_id","primary_flag"]},"rows":{"Employee":[["1","1","N"],["2","1","Y"],["2","2","N"],["3","3","N"],["4","2","N"],["4","3","Y"],["4","4","N"]]}}
+# Output
+# {"headers": ["employee_id", "department_id"], "values": [[1, 1], [2, 1], [3, 3], [4, 2]]}
+# Expected
+# {"headers":["employee_id","department_id"],"values":[[1,1],[2,1],[3,3],[4,3]]}
+
