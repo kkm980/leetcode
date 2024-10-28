e92f7e12d36f2357c16c7f4ef42615424ea95f0f
KRISHNA KANT <77038999+kkm980@users.noreply.github.com>
2024-10-29 00:42:13 +0530

    Time: 681 ms (33.96%), Space: 0B (100.00%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -2,7 +2,7 @@
 SELECT 
     employee_id,
     COALESCE(
-        MAX(CASE WHEN primary_flag = 'Y' THEN department_id END),
+        MIN(CASE WHEN primary_flag = 'Y' THEN department_id END),
         department_id
     ) AS department_id
 FROM Employee
