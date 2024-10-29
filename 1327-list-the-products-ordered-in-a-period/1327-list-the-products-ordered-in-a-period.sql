075d64d16c7c5d986e284fe4f2b0be5bdf84aba3
KRISHNA KANT <77038999+kkm980@users.noreply.github.com>
2024-10-30 02:03:19 +0530

    Time: 759 ms (69.30%), Space: 0B (100.00%) - LeetHub

--- 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
+++ 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
@@ -1,8 +1,17 @@
 # Write your MySQL query statement below
+# SELECT p.product_name, 
+# sum(if(O.order_date like "2020-02-%",o.unit,0)) AS UNIT
+# FROM Products as p
+# RIGHT JOIN Orders as o
+# ON p.product_id=o.product_id
+# GROUP BY o.product_id
+# HAVING UNIT >= 100;
+
 SELECT p.product_name, 
-sum(if(O.order_date like "2020-02-%",o.unit,0)) AS UNIT
-FROM Products as p
-RIGHT JOIN Orders as o
-ON p.product_id=o.product_id
-GROUP BY o.product_id
+       SUM(IF(o.order_date LIKE '2020-02-%', o.unit, 0)) AS UNIT
+FROM Products AS p
+RIGHT JOIN Orders AS o
+ON p.product_id = o.product_id
+GROUP BY p.product_name
 HAVING UNIT >= 100;
+
