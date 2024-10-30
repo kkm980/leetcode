2c191f122ed9f774581fbac5ed2becc7d2ac981d
KRISHNA KANT <77038999+kkm980@users.noreply.github.com>
2024-10-31 02:26:05 +0530

    Time: 1294 ms (40.67%), Space: 0B (100.00%) - LeetHub

---
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,20 @@
+# Write your MySQL query statement below
+WITH CumulativeQueue AS (
+    SELECT
+        person_name,
+        weight,
+        SUM(weight) OVER (ORDER BY turn) AS cumulative_weight
+    FROM
+        Queue
+    ORDER BY
+        turn
+)
+SELECT
+    person_name
+FROM
+    CumulativeQueue
+WHERE
+    cumulative_weight <= 1000
+ORDER BY
+    cumulative_weight DESC
+LIMIT 1;
