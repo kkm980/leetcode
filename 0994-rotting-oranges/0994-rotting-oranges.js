43ee0a6f8e6f59c6f2c9fef28a6cfd33d0019b62
KRISHNA KANT <77038999+kkm980@users.noreply.github.com>
2024-12-19 14:37:40 +0530

    Time: 45 ms (6.69%), Space: 58.7 MB (5.32%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var orangesRotting = function(grid) {
+    let time = 0
+    const queue = []
+    const m = grid.length
+    const n = grid[0].length
+    for(let i = 0; i < m; i++){
+        for(let j = 0; j < n; j++){
+            if(grid[i][j] === 2){
+                queue.push([i,j])
+            }
+        }
+    }
+    while(queue.length > 0){
+        const size = queue.length
+        time += 1
+        for(let k = 0; k < size; k++){
+            const [x, y] = queue.shift()
+            for(let [i,j] of [[0,1],[0,-1],[1,0],[-1,0]]){
+                if(x+i < 0 || x+i >= m || y+j < 0 || y+j >= n || grid[x+i][y+j] === 0 || grid[x+i][y+j] === 2)continue
+                if(grid[x+i][y+j] === 1){
+                    grid[x+i][y+j] = 2
+                    queue.push([x+i,y+j])
+                }
+            }
+        }
+    }
+    console.log(grid)
+    for(let i = 0; i < m; i++){
+        for(let j = 0; j < n; j++){
+            if(grid[i][j] === 1)return -1
+        }
+    }
+    return time === 0 ? 0 : time-1
+};
