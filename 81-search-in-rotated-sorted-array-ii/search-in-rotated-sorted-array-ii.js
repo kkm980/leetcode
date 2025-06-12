6c453dcc8b2ae9d463d298ff0e1de42394bd7178
KRISHNA KANT <77038999+kkm980@users.noreply.github.com>
2025-06-12 13:34:31 +0530

    Time: 0 ms (100.00%) | Memory: 53.4 MB (88.93%) - LeetSync

---
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {boolean}
+ */
+var search = function(nums, target) {
+    if(nums.indexOf(target)===-1){
+        return false
+    } else{
+        return true;
+    }
+};
