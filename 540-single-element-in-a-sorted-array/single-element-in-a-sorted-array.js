58def98809c566a777a82a1aa2bf7f46962f007c
KRISHNA KANT <77038999+kkm980@users.noreply.github.com>
2025-06-12 15:36:36 +0530

    Time: 0 ms (100.00%) | Memory: 56.7 MB (81.75%) - LeetSync

---
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNonDuplicate = function(nums) {
+    let left = 0, right = nums.length - 1;
+    while (left < right) {
+        let mid = Math.floor((left + right) / 2);
+        if (mid % 2 === 1) mid--;
+        if (nums[mid] !== nums[mid + 1]) {
+            right = mid;
+        } else {
+            left = mid + 2;
+        }
+    }
+    return nums[left];
+};
+
+
