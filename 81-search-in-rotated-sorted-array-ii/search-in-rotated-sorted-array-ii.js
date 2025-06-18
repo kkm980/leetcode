939d155c6107d3187d6e2ce7bfe129a57340430d
KRISHNA KANT <77038999+kkm980@users.noreply.github.com>
2025-06-18 22:37:35 +0530

    Time: 0 ms (100.00%) | Memory: 53 MB (94.60%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.js
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.js
@@ -4,24 +4,58 @@
  * @return {boolean}
  */
 var search = function(nums, target) {
+    // let low = 0, high = nums.length - 1;
+
+    // while (low <= high) {
+    //     let mid = Math.floor((low + high) / 2);
+    //     if (nums[mid] === target) return true;
+
+    //     if (nums[low] === nums[mid]) {
+    //         low++;
+    //         continue;
+    //     }
+
+    //     if (nums[low] <= nums[mid]) {
+    //         if (nums[low] <= target && target <= nums[mid]) high = mid - 1;
+    //         else low = mid + 1;
+    //     } else {
+    //         if (nums[mid] <= target && target <= nums[high]) low = mid + 1;
+    //         else high = mid - 1;
+    //     }
+    // }
+    // return false;
     let low = 0, high = nums.length - 1;
 
     while (low <= high) {
         let mid = Math.floor((low + high) / 2);
-        if (nums[mid] === target) return true;
 
-        if (nums[low] === nums[mid]) {
-            low++;
-            continue;
+        if (nums[mid] === target) {
+            return true;
         }
 
-        if (nums[low] <= nums[mid]) {
-            if (nums[low] <= target && target <= nums[mid]) high = mid - 1;
-            else low = mid + 1;
-        } else {
-            if (nums[mid] <= target && target <= nums[high]) low = mid + 1;
-            else high = mid - 1;
+        // If duplicates on both sides, shrink the search space
+        if (nums[low] === nums[mid] && nums[mid] === nums[high]) {
+            low++;
+            high--;
+        }
+        // Left half is sorted
+        else if (nums[low] <= nums[mid]) {
+            if (nums[low] <= target && target < nums[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        // Right half is sorted
+        else {
+            if (nums[mid] < target && target <= nums[high]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
         }
     }
+
     return false;
+
 }
