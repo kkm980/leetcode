74f08cebf95d360c78377509a3070fdd83daa11a
KRISHNA KANT <77038999+kkm980@users.noreply.github.com>
2025-06-29 16:16:56 +0530

    Time: 27 ms (28.08%) | Memory: 68.7 MB (35.73%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.js
+++ 287-find-the-duplicate-number/find-the-duplicate-number.js
@@ -2,39 +2,41 @@
  * @param {number[]} nums
  * @return {number}
  */
-// var findDuplicate = function(nums) {
-//     let low = 1;
-//     let high = nums.length - 1;
-
-//     while (low < high) {
-//         let mid = Math.floor((low + high) / 2);
-
-//         // Count how many numbers are ≤ mid
-//         let count = 0;
-//         for (let num of nums) {
-//             if (num <= mid) count++;
-//         }
-
-//         // If count > mid, duplicate must be in [low, mid]
-//         if (count > mid) {
-//             high = mid;
-//         } else {
-//             low = mid + 1;
-//         }
-//     }
-
-//     // low == high is the duplicate number
-//     return low;
-// };
 var findDuplicate = function(nums) {
-    for(let i = 0; i < nums.length; i++){
-        let ind = Math.abs(nums[i]);
-        if(nums[ind] < 0){
-            return ind;
+    let low = 1;
+    let high = nums.length - 1;
+    let answer = -1; // Will store the potential duplicate
+
+    while (low <= high) {
+        const mid = Math.floor((low + high) / 2);
+
+        // Count how many numbers are <= mid
+        let count = 0;
+        for (const num of nums) {
+            if (num <= mid) count++;
         }
 
-        nums[ind] = - nums[ind];
+        if (count > mid) {
+            // mid is a potential answer
+            answer = mid;
+            high = mid - 1; // Search left half
+        } else {
+            low = mid + 1; // Search right half
+        }
     }
 
-    return -1;
+    return answer;
 };
+
+// var findDuplicate = function(nums) {
+//     for(let i = 0; i < nums.length; i++){
+//         let ind = Math.abs(nums[i]);
+//         if(nums[ind] < 0){
+//             return ind;
+//         }
+
+//         nums[ind] = - nums[ind];
+//     }
+
+//     return -1;
+// };
