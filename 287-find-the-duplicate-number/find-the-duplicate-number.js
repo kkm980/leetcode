7cca29526fd4ca659a9da7274cefee1f908ebdf8
KRISHNA KANT <77038999+kkm980@users.noreply.github.com>
2025-06-29 16:03:20 +0530

    Time: 0 ms (100.00%) | Memory: 62.6 MB (91.84%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.js
+++ 287-find-the-duplicate-number/find-the-duplicate-number.js
@@ -2,27 +2,39 @@
  * @param {number[]} nums
  * @return {number}
  */
+// var findDuplicate = function(nums) {
+//     let low = 1;
+//     let high = nums.length - 1;
+
+//     while (low < high) {
+//         let mid = Math.floor((low + high) / 2);
+
+//         // Count how many numbers are ≤ mid
+//         let count = 0;
+//         for (let num of nums) {
+//             if (num <= mid) count++;
+//         }
+
+//         // If count > mid, duplicate must be in [low, mid]
+//         if (count > mid) {
+//             high = mid;
+//         } else {
+//             low = mid + 1;
+//         }
+//     }
+
+//     // low == high is the duplicate number
+//     return low;
+// };
 var findDuplicate = function(nums) {
-    let low = 1;
-    let high = nums.length - 1;
-
-    while (low < high) {
-        let mid = Math.floor((low + high) / 2);
-
-        // Count how many numbers are ≤ mid
-        let count = 0;
-        for (let num of nums) {
-            if (num <= mid) count++;
+    for(let i = 0; i < nums.length; i++){
+        let ind = Math.abs(nums[i]);
+        if(nums[ind] < 0){
+            return ind;
         }
 
-        // If count > mid, duplicate must be in [low, mid]
-        if (count > mid) {
-            high = mid;
-        } else {
-            low = mid + 1;
-        }
+        nums[ind] = - nums[ind];
     }
 
-    // low == high is the duplicate number
-    return low;
+    return -1;
 };
