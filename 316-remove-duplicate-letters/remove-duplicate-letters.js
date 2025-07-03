e3cf888d27e1bcc66cc5f41d88a69d410136e471
KRISHNA KANT <77038999+kkm980@users.noreply.github.com>
2025-07-03 14:38:20 +0530

    Time: 0 ms (100.00%) | Memory: 54.5 MB (70.18%) - LeetSync

--- 316-remove-duplicate-letters/remove-duplicate-letters.js
+++ 316-remove-duplicate-letters/remove-duplicate-letters.js
@@ -7,54 +7,30 @@
  * @return {string}
  */
 var removeDuplicateLetters = function(s) {
-    var countObject = {};//contains each letters count in string
-    var isInStack = {};//tells whether a letter is already in the stack or not
-    var stack = [];//the final stack which will contain the string
-    
-    //build countObject and initialize isInStack
-    for(var i=0; i<s.length; i++){
-        if(!countObject[s[i]]){
-            countObject[s[i]] = 0;
-        }
-        countObject[s[i]]++;
-        isInStack[s[i]] = false;
+    const lastOccurrence = {};
+    for (let i = 0; i < s.length; i++) {
+        lastOccurrence[s[i]] = i;
     }
-    
-    //iterate through string
-    for(var i=0; i<s.length; i++){
-        var currentLetter = s[i];
-        var stackTop = stack[stack.length - 1];
-        
-        //decrement counter for every letter each time we see it
-        countObject[currentLetter]--;
-        
-        if(isInStack[currentLetter]){
-            //currentLetter is already in stack. Skip it.
+
+    const stack = [];
+    const visited = new Set();
+
+    for (let i = 0; i < s.length; i++) {
+        if (visited.has(s[i])) {
             continue;
         }
-        
-        if(currentLetter < stackTop){
-            //currentLetter comes first lexicographically
-            //do more of the stacktop element exist further in the string?
-            //(we can find this out from countObject)
-            //keep popping while currentLetter < stackTop
-            //or if stackTop does not exist further in the string
-            while(currentLetter < stackTop && countObject[stackTop] > 0){
-                isInStack[stack.pop()] = false;
-                stackTop = stack[stack.length - 1];
-            }
-            
-            //the stack top should now have a letter which is either lexicographically
-            //smaller than currentLetter or 
-            //a letter which does not have any more occurrences in the string
-            //for example in bbacd, when currentLetter is a, stack top would be b
+
+        while (
+            stack.length > 0 &&
+            s[i] < stack[stack.length - 1] &&
+            i < lastOccurrence[stack[stack.length - 1]]
+        ) {
+            visited.delete(stack.pop());
         }
-        
-        //push the currentLetter to stack
-        isInStack[currentLetter] = true;
-        stack.push(currentLetter);
+
+        visited.add(s[i]);
+        stack.push(s[i]);
     }
-    
-    //convert stack arrray to string and return
-    return stack.join('');
-}
+
+    return stack.join('');    
+};
