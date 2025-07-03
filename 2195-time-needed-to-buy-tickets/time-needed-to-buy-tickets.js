4883d4425f945c12ed6a5f94cba71d959fe58a5e
KRISHNA KANT <77038999+kkm980@users.noreply.github.com>
2025-07-03 11:31:05 +0530

    Time: 0 ms (100.00%) | Memory: 53.7 MB (68.88%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.js
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.js
@@ -3,30 +3,44 @@
  * @param {number} k
  * @return {number}
  */
+// var timeRequiredToBuy = function(tickets, k) {
+//     let queue = [];
+    
+//     // Initialize queue with [index, tickets[i]]
+//     for (let i = 0; i < tickets.length; i++) {
+//         queue.push([i, tickets[i]]);
+//     }
+    
+//     let time = 0;
+    
+//     while (queue.length > 0) {
+//         let [index, ticketCount] = queue.shift(); // Person at front buys one ticket
+//         ticketCount -= 1;
+//         time++;
+        
+//         if (ticketCount > 0) {
+//             queue.push([index, ticketCount]); // Goes to end of line
+//         }
+        
+//         if (index === k && ticketCount === 0) {
+//             return time; // Our person finished
+//         }
+//     }
+    
+//     // return time;
+// };
+
+
 var timeRequiredToBuy = function(tickets, k) {
-    let queue = [];
-    
-    // Initialize queue with [index, tickets[i]]
-    for (let i = 0; i < tickets.length; i++) {
-        queue.push([i, tickets[i]]);
-    }
-    
     let time = 0;
-    
-    while (queue.length > 0) {
-        let [index, ticketCount] = queue.shift(); // Person at front buys one ticket
-        ticketCount -= 1;
-        time++;
-        
-        if (ticketCount > 0) {
-            queue.push([index, ticketCount]); // Goes to end of line
-        }
-        
-        if (index === k && ticketCount === 0) {
-            return time; // Our person finished
+    for (let i = 0; i < tickets.length; i++) {
+        if (i <= k) {
+            time += Math.min(tickets[i], tickets[k]);
+        } else {
+            time += Math.min(tickets[i], tickets[k] - 1);
         }
     }
-    
-    // return time;
+    return time;
 };
+
 
