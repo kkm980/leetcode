17f20fe6bb4c23b1674fa626fd87e61bfc619656
KRISHNA KANT <77038999+kkm980@users.noreply.github.com>
2025-07-03 11:16:29 +0530

    Time: 6 ms (18.67%) | Memory: 56.4 MB (13.28%) - LeetSync

---
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} tickets
+ * @param {number} k
+ * @return {number}
+ */
+var timeRequiredToBuy = function(tickets, k) {
+    let queue = [];
+    
+    // Initialize queue with [index, tickets[i]]
+    for (let i = 0; i < tickets.length; i++) {
+        queue.push([i, tickets[i]]);
+    }
+    
+    let time = 0;
+    
+    while (queue.length > 0) {
+        let [index, ticketCount] = queue.shift(); // Person at front buys one ticket
+        ticketCount -= 1;
+        time++;
+        
+        if (ticketCount > 0) {
+            queue.push([index, ticketCount]); // Goes to end of line
+        }
+        
+        if (index === k && ticketCount === 0) {
+            return time; // Our person finished
+        }
+    }
+    
+    // return time;
+};
+
