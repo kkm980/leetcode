f7f5a8e30b160513d22fadf047872df7a9d415a2
KRISHNA KANT <77038999+kkm980@users.noreply.github.com>
2025-07-06 22:27:27 +0530

    Time: 44 ms (68.44%) | Memory: 53.5 MB (52.75%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.js
+++ 1013-fibonacci-number/fibonacci-number.js
@@ -3,8 +3,13 @@
  * @return {number}
  */
 var fib = function(n) {
-    if(n<=1){
-        return n;
+    let fib = [0, 1];
+    for(let i=2; i<=n; i++){
+        fib.push(fib[i-1] + fib[i-2]);
     }
-    return fib(n-1)+fib(n-2);
+    return fib[n];
+    // if (n<=1){
+    //     return n;
+    // }
+    // return fib(n-1) + fib(n-2);
 };
