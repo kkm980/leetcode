c6890f0f3942f3b103e0d26484489098878c95ad
KRISHNA KANT <77038999+kkm980@users.noreply.github.com>
2025-07-07 20:06:39 +0530

    Time: 0 ms (100.00%) | Memory: 54.2 MB (20.16%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.js
+++ 226-invert-binary-tree/invert-binary-tree.js
@@ -11,9 +11,12 @@
  * @return {TreeNode}
  */
 var invertTree = function(root) {
-    if (!root) return null;
-    [root.left, root.right] = [root.right, root.left];
-    invertTree(root.left);
-    invertTree(root.right);
-    return root;
+    if(!root){
+        return null
+    }
+    const left=invertTree(root.left);
+    const right=invertTree(root.right);
+    root.left=right;
+    root.right=left;
+    return root
 };
